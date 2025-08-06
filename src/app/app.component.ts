61a9a959297ca7d9fd7403b60e1413665a456288
YuriZem <yurizem333@gmail.com>
2025-08-06 15:12:27 -0300

    Adição do banco de dados local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-
+import { Storage } from '@ionic/storage-angular';
+import { StorageService } from './services/storage.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet,],
+  providers: [Storage, StorageService] // Adicione aqui
 })
 export class AppComponent {
   constructor() {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {IonButton, IonHeader, IonToolbar, IonTitle, IonContent,IonItem, IonList,IonCol,IonRow,IonGrid} from '@ionic/angular/standalone';
+import { StorageService } from '../services/storage.service';
+
+// import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton,IonHeader, IonToolbar, IonTitle, IonContent,IonItem, IonList,IonCol,IonRow,IonGrid,CommonModule],
+  // providers: [Storage]
 })
 export class HomePage {
-  constructor() {}
+items = [
+  { name: 'Item 1', visible: true },
+  { name: 'Item 2', visible: false },
+  { name: 'Item 3', visible: true }
+];
+  constructor(
+    private storageService: StorageService
+  ) {}
+
+  async ngOnInit() {
+    await this.storageService.init();
+    await this.storageService.set('PRODUTOS',this.items);
+    const valor = await this.storageService.get('key');
+    console.log(valor);
+  }
 }
